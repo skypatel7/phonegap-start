31bbf893f7ea2c75386689c6dad57e39c320c51f
skypatel7 <skypatel7@gmail.com>
2013-03-20 16:08:55 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,7 @@ var app = {
     deviceready: function() {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
-        app.report('deviceready');
+        window.location.href="http://ptwtesting.skyproductivity.com/login/Android_loging.aspx";
     },
     report: function(id) {
         // Report the event in the console
